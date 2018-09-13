52bda7514f15a00937194ab9082119ce633fb9bc
Nenivar <nenivar@gmail.com>
2018-09-13 15:28:38 +0100

    more dynamic stuff

--- app/proc.rb
+++ app/proc.rb
@@ -13,12 +13,18 @@ def readImg(path)
   Magick::Image.read('assets/' + path).first
 end
 
-def drawText!(base, text)
+def drawText!(base, text, col)
   t = Draw.new
   t.font_family = 'helvetica'
   t.pointsize = 52
   t.gravity = CenterGravity
-  t.annotate(base, 0, 0, 0, 0, text)
+  t.annotate(base, 0, 0, 0, 0, text) { self.fill = col }
+end
+
+QUANT_MULT = 257
+
+def arrToPixel(arr)
+  Pixel.new(arr[0] * QUANT_MULT, arr[1] * QUANT_MULT, arr[2] * QUANT_MULT, 255)
 end
 
 def createEventImg(event_id)
@@ -26,17 +32,17 @@ def createEventImg(event_id)
   size = Config.getConfig()['image_size']
 
   catData = Config.getConfig()['categories'][data['category']]
-  bg = catData['bg']
 
-  f = createBlankImg(size[0], size[1], Pixel.new(bg[0], bg[1], bg[2], 255))
+  f = createBlankImg(size[0], size[1], 'white')
   icon = readImg(catData['icon'])
   icon.resize_to_fit!(size[0]/2, size[1]/2)
   f.composite!(icon, CenterGravity, OverCompositeOp)
-  drawText!(f, data['name'])
+  drawText!(f, data['name'], arrToPixel(catData['text']))
   f.write('test.png')
 end
 
-createEventImg('welcome-drinks-2018')
+#createEventImg('welcome-drinks-2018')
+createEventImg('test-talk-2018')
 
 =begin def combine(imgs)
   all = ImageList.new(img[0], img[1]...)
