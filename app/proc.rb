e27bc61fdee171e9ff64a1792ad05d9a410c821c
Nenivar <nenivar@gmail.com>
2018-09-13 14:34:13 +0100

    reusable img methods

--- app/proc.rb
+++ app/proc.rb
@@ -1,21 +1,32 @@
 require 'rmagick'
 include Magick
 
+def createBlankImg(width, height, bg_col)
+  Image.new(width, height) { self.background_color = bg_col }
+end
 
+def readImg(path)
+  Magick::Image.read("assets/icons/social.png").first
+end
 
-f = Image.new(500, 500) { self.background_color = "white" }
-icon = Magick::Image.read("assets/icons/social.png").first
-icon.resize_to_fit!(50, 100)
-g = f.composite(icon, CenterGravity, OverCompositeOp)
+def drawText!(base, text)
+  t = Draw.new
+  t.font_family = 'helvetica'
+  t.pointsize = 52
+  t.gravity = CenterGravity
+  t.annotate(base, 0, 0, 0, 0, text)
+end
 
-#text = 'dog'
-t = Draw.new
-t.font_family = 'helvetica'
-t.pointsize = 52
-t.gravity = CenterGravity
-t.annotate(g, 0, 0, 0, 0, 'dog')
+def createEventImg(event_id)
+  f = createBlankImg(500, 500, 'white')
+  icon = readImg("assets/icons/social.png")
+  icon.resize_to_fit!(50, 100)
+  g = f.composite(icon, CenterGravity, OverCompositeOp)
+  drawText!(g, 'dog')
+  g.write('test.png')
+end
 
-g.write('test.png')
+createEventImg('dog')
 
 =begin def combine(imgs)
   all = ImageList.new(img[0], img[1]...)
