2f6e15815fc39081b096d03c573dbe307e36675b
Nenivar <nenivar@gmail.com>
2018-09-13 14:18:57 +0100

    text overlay

--- app/proc.rb
+++ app/proc.rb
@@ -6,13 +6,16 @@ include Magick
 f = Image.new(500, 500) { self.background_color = "white" }
 icon = Magick::Image.read("assets/icons/social.png").first
 icon.resize_to_fit!(50, 100)
-f.composite(icon, CenterGravity, OverCompositeOp).write('test.png')
+g = f.composite(icon, CenterGravity, OverCompositeOp)
 
 #text = 'dog'
-#t = Draw.new
-#t.font_family = 'helvetica'
-#t.pointsize = 52
-#t.gravity = CenterGravity
+t = Draw.new
+t.font_family = 'helvetica'
+t.pointsize = 52
+t.gravity = CenterGravity
+t.annotate(g, 0, 0, 0, 0, 'dog')
+
+g.write('test.png')
 
 =begin def combine(imgs)
   all = ImageList.new(img[0], img[1]...)
