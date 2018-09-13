fbbc0f5019ad4a4e8878fb5fb1b33ea5f7ee17ae
Nenivar <nenivar@gmail.com>
2018-09-13 13:27:13 +0100

    rmagick install & test

--- app/core.rb
+++ app/core.rb
@@ -1,5 +1,21 @@
 require 'json'
+require 'rmagick'
+include Magick
 
-path = File.expand_path('../../data/events/welcome-drinks-2018.json', __FILE__)
+DATA_PATH = '../../data/'
+EVENTS_PATH = 'events/'
+OUTPUT_PATH = '../../output/'
+
+path = File.expand_path(DATA_PATH + EVENTS_PATH + 'welcome-drinks-2018.json', __FILE__)
 obj = JSON.parse(File.read(path))
 print(obj)
+
+f = Image.new(100, 100) { self.background_color = "white" }
+f.write('test.png')
+
+=begin def combine(imgs)
+  all = ImageList.new(img[0], img[1]...)
+  all.write('etc.png')
+  exit
+end
+=end
